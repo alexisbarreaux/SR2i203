e5d6aedd64270e31182c52b7e48a8703a0fd5faa
Alexis <alexis.barreaux@gmail.com>
2021-01-27 12:01:37 +0100

    Building requirements and starting python

---
+++ main.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on January 27, 2021
+@author: Charlie Bailly <charlie.bailly@telecom-paris.fr>
+@author: Alexis Barreaux <alexis.barreaux@telecom-paris.fr>
+"""
+
+# scapy documentation : https://scapy.readthedocs.io/en/latest/
+
+import scapy
+
+"""
+scapy
+conf.color_theme = BlackAndWhite
+IP()
+target="www.target.com/30"
+ip=IP(dst=target)
+[p for p in ip]
+quit()
+"""
+
+
