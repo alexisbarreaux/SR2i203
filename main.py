b96fb450e79cbf508f0c4cd046fd048b3ce99a3e
Alexis <alexis.barreaux@gmail.com>
2021-01-28 18:43:24 +0100

    Small progress
    Just trying to find how to use TLS with scapy

--- main.py
+++ main.py
@@ -7,17 +7,43 @@ Created on January 27, 2021
 """
 
 # scapy documentation : https://scapy.readthedocs.io/en/latest/
+# sous windows il faut installer npcap
+#  a, b = sr(requête) mets les requêtes qui ont eu une réponse dans a et les autres dans b
+# (1,20) = de 1 à 20
 
 import scapy
+from scapy.layers.http import HTTP, HTTPRequest
+from scapy.layers.inet import IP, TCP
+from scapy.layers.l2 import Ether
+from scapy.layers.tls.automaton_cli import TLSClientAutomaton
+from scapy.layers.tls.handshake import TLSClientHello
+# TODO il faut trouver comment ce servir de ça
+from scapy.layers.tls.extensions import TLS_Ext_Heartbeat
+from scapy.main import load_layer
+from scapy.sendrecv import sniff, sendp
 
-"""
-scapy
-conf.color_theme = BlackAndWhite
-IP()
-target="www.target.com/30"
-ip=IP(dst=target)
-[p for p in ip]
-quit()
-"""
+from scapy.sessions import TCPSession
+# TODO apparemment TCPSession permet de gérer un peu le TLS, TLSSession sinon
+
+# Ajout de TLS
+load_layer('tls')
 
 
+def test_pkt(dst="127.0.0.1"):
+    trame = Ether()
+    ip = IP(src="8.8.8.8", dst=dst)
+    tcp = TCP(dport=443, flag="S")
+
+    test = trame / ip / tcp
+    sendp(test)
+    return
+
+
+def https_test():
+    # dans a on peut utiliser version"sslv2" ou "tls1" ou "tls12" pour faire des tests.
+    # data sert à envoyer des données après que le lien soit fait
+    a = TLSClientAutomaton.tlslink(HTTP, server="www.google.com", dport=443)
+    pkt = a.sr1(HTTP() / HTTPRequest(), session=TCPSession(app=True), timeout=2)
+    pkt.show()
+
+    return
