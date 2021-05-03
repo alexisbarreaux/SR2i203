67394e7ffbd01358e646ff1c8c3c5285aeaf5189
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2021-05-03 15:42:47 +0200

    Implemented addition and inverse on elliptic curves

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import numpy as np
 
+
 # https://en.wikipedia.org/wiki/Lenstra_elliptic-curve_factorization#Algorithm
+# https://stackoverflow.com/questions/31074172/elliptic-curve-point-addition-over-a-finite-field-in-python
+
+
+def valid_point(x: int, y: int, a: int, b: int, n: int):
+    return (pow(y, 2) % n) == ((pow(x, 3) + a * x + b) % n)
 
 
 def create_random_point(n: int):
@@ -25,4 +31,43 @@ def create_random_elliptic(n: int):
     a = np.random.randint(n)
     b = (pow(y_0, 2, n) - pow(x_0, 3, n) - a * x_0) % n
     return [a, b, x_0, y_0]
+
+
+def inv_mod_n(n: int, x: int):
+    assert (np.gcd(n, x) == 1)
+
+    # Using fermat theorem + gcd(n, x) == 1
+    return pow(x, n - 2, n)
+
+
+def elliptic_inverse(n: int, x_p: int, y_p: int):
+    return x_p, (-y_p) % n
+
+
+def elliptic_addition(n: int, a: int, b:int, x_p: int, y_p: int, x_q: int, y_q: int):
+    assert (0 <= x_p < n and 0 <= x_q < n and 0 <= y_p < n and 0 <= y_q < n)
+
+    if x_p != x_q:
+        # Compute the slope and the ordinate of the line between p and q
+        u = (y_p - y_q) % n
+        v = (x_p - x_q)
+    elif y_p != 0:
+        u = (3 * pow(x_p, 2) + a) % n
+        v = (2 * y_p) % n
+    else:
+        # Either P, Q or P + Q is the origin which we are not interested in.
+        return 2, []
+
+    try:
+        v = inv_mod_n(n, v)
+    except Exception as e:
+        # v is a factor of n
+        return 0, [np.gcd(n, v)]
+
+    s = (u * v) % n
+    x_r = (pow(s, 2) - x_p - x_q) % n
+    y_r = (y_p + s * (x_r - x_p)) % n
+    assert(valid_point(x_r, y_r, a, b, n))
+    return 1, [x_r, y_r]
+
 
