e480d6ce12a9e998da1ab7162c09159b53164346
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2021-05-10 15:31:05 +0200

    added a quick multiplication

--- main.py
+++ main.py
@@ -105,30 +105,32 @@ def elliptic_multiplication(n: int, a: int, b: int, x: int, y: int, k: int):
     else:
         if k % 2 == 0:
             # We want to return k//2 * P + k//2 * P
-            ret_code, l = elliptic_multiplication(n, a, b, x, y, n//2)
+            ret_code, l = elliptic_multiplication(n, a, b, x, y, k//2)
 
             # However we must check if k//2 * P is indeed a true point or not.
             if ret_code == 1:
                 x2, y2 = l
-                return elliptic_addition(n, a, b, x, y)
+                return elliptic_addition(n, a, b, x2, y2)
             else:
                 return ret_code, l
 
         else:
             # We want to return P * k//2 * P + k//2 * P
-            ret_code, l = elliptic_multiplication(n, a, b, x, y, (n - 1) // 2)
+            ret_code, l = elliptic_multiplication(n, a, b, x, y, (k - 1) // 2)
             if ret_code == 1:
                 x2, y2 = l
                 ret_code2, l2 = elliptic_addition(n, a, b, x2, y2)
                 if ret_code2 == 1:
                     x3, y3 = l2
                     return elliptic_addition(n, a, b, x, y, x3, y3)
+                else:
+                    return ret_code2, l2
             else:
                 return ret_code, l
 
 
 def lenstra(n: int):
-    # TODO add fast calculus of k * P where P is a point on the curve
+
     a, b, x, y = create_random_elliptic(n)
 
     ret_code, res = elliptic_addition(n, a, b, x, y)
