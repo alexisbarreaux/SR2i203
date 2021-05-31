37a1d07c7ac666b90c8ae80bc7b9f89adb203a7a
Charlie Bailly <charliebailly29@gmail.com>
2021-05-31 18:35:41 +0200

    Add a very simple bruteforce

---
+++ main_charlie.py
@@ -0,0 +1,13 @@
+from address_generation import getCompressedPublicKey
+
+
+def bruteforce(compressedPubKey, min=1, max=2**256-1):
+    for privKey in range(min, max):
+        if getCompressedPublicKey(privKey.to_bytes(32, "big")) == compressedPubKey:
+            return privKey
+
+
+if __name__ == '__main__':
+    print(bruteforce("19ZewH8Kk1PDbSNdJ97FP4EiCjTRaZMZQA"))
+    print(bruteforce("1EhqbyUMvvs7BfL8goY6qcPbD6YKfPqb7e"))
+    print(bruteforce("1CQFwcjw1dwhtkVWBttNLDtqL7ivBonGPV"))
